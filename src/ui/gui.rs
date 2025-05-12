c2c2d1f0cd3ec5d1cb2ae539626621a15432eeab
Matthew Fay <mmfay3@gmail.com>
2025-05-12 03:59:54 -0700

    Add PEMDAS logic

--- src/ui/gui.rs
+++ src/ui/gui.rs
@@ -9,7 +9,7 @@ pub struct MyApp {
     current_number: String,
     queue: VecDeque<String>,
     stack_op: Vec<String>,
-    stack_nums: Vec<i32>,
+    stack_nums: Vec<f64>,
 }
 
 impl App for MyApp {
@@ -27,6 +27,7 @@ impl App for MyApp {
                     self.current_number.push_str("9");
                 }
                 if ui.add_sized([40.0,40.0], egui::Button::new("x")).clicked() {
+                    self.operation();
                     self.queue.push_back("x".to_string());
                 }
             });
@@ -41,6 +42,7 @@ impl App for MyApp {
                     self.current_number.push_str("6");
                 }
                 if ui.add_sized([40.0,40.0], egui::Button::new("-")).clicked() {
+                    self.operation();
                     self.queue.push_back("-".to_string());
                 }
             });
@@ -55,6 +57,7 @@ impl App for MyApp {
                     self.current_number.push_str("3");
                 }
                 if ui.add_sized([40.0,40.0], egui::Button::new("+")).clicked() {
+                    self.operation();
                     self.queue.push_back("+".to_string());
                 }
             });
@@ -66,9 +69,11 @@ impl App for MyApp {
                     self.current_number.push_str(".");
                 }
                 if ui.add_sized([40.0,40.0], egui::Button::new("=")).clicked() {
+                    self.operation();
                     self.equal();
                 }
                 if ui.add_sized([40.0,40.0], egui::Button::new("/")).clicked() {
+                    self.operation();
                     self.queue.push_back("/".to_string());
                 }
             });
@@ -81,9 +86,91 @@ impl App for MyApp {
 impl MyApp {
     fn equal(&mut self) {
         println!("triggering calc");
-        while self.queue.len() > 0 {
-            let tmp = self.queue.pop_front();
-            println!("{:?}",tmp);
+
+        // clear old stacks
+        self.stack_nums.clear();
+        self.stack_op.clear();
+
+        // fill stacks from queue into separate buckets of ops and nums
+        while let Some(token) = self.queue.pop_front() {
+            if let Ok(num) = token.parse::<f64>() {
+                self.stack_nums.push(num);
+            } else {
+                self.stack_op.push(token);
+            }
         }
+
+        // First pass: handle multiplication and division
+        let mut nums = vec![self.stack_nums[0]];
+        let mut ops = vec![];
+
+        // i = index, op = element (operation), multiplication and division section
+        for (i, op) in self.stack_op.iter().enumerate() {
+
+            let b = self.stack_nums[i + 1];
+
+            match op.as_str() {
+
+                // if op is multiplication, take a * b and push to nums.
+                "x" => {
+
+                    let a = nums.pop().unwrap();
+                    nums.push(a * b);
+
+                },
+
+                // if op is division, take a / b and push to nums.
+                "/" => {
+
+                    if b == 0.0 {
+                        println!("Division by zero!");
+                        return;
+                    }
+                    let a = nums.pop().unwrap();
+                    nums.push(a / b);
+
+                },
+
+                // if addition or subtraction, dont do op until all multiplication and division yet
+                "+" | "-" => {
+
+                    nums.push(b);
+                    ops.push(op.clone());
+
+                },
+
+                // in case any new operator is added and not handled
+                _ => {
+
+                    println!("Unknown operator: {}", op);
+                    return;
+
+                }
+
+            }
+        }
+
+        // i = index, op = element (operation), addition and subtraction section
+        let mut result = nums[0];
+        for (i, op) in ops.iter().enumerate() {
+            let b = nums[i + 1];
+            match op.as_str() {
+                "+" => result += b,
+                "-" => result -= b,
+                _ => unreachable!(),
+            }
+        }
+
+        println!("Result: {}", result);
+
+        // Show result and reset
+        self.current_number = result.to_string();
+        self.queue.clear();
+        self.stack_nums.clear();
+        self.stack_op.clear();
+    }
+    fn operation(&mut self) {
+        self.queue.push_back(self.current_number.clone());
+        self.current_number = "".to_string();
     }
 }
